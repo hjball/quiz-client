d65379a0ee5c102c078cfa6c7eb2b9f2ea6a4002
Max McCormick <maxmccormick86@gmail.com>
2018-12-19 16:45:52 +0000

    added goback funcyionality

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Result = ({ result }) => (
 	<div className="container d-flex" style={{height: 400, backgroundColor: "grey", alignItems: "center", justifyContent: "center"}}>
 		<h2>You got { result.result } out of { result.outOf }</h2>
-		<a href="#">Go Back</a>
+		<Link to="/">Go Back</Link>
 	</div>
 );
 
